72691687ef750615088a93b0d34fbd411666bb09
masjidnuruttaqwa01-ship-it <masjidnuruttaqwa01@gmail.com>
2026-01-05 23:49:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,31 @@
-const CACHE_NAME = "nurut-taqwa-v1";
-const urlsToCache = [
-  "/masjid-nurut-taqwa/",
-  "/masjid-nurut-taqwa/index.html"
+const CACHE_NAME = "nurut-taqwa-v2";
+const OFFLINE_URL = "/masjid-nurut-taqwa/index.html";
+
+const FILES_TO_CACHE = [
+  OFFLINE_URL,
+  "/masjid-nurut-taqwa/manifest.json",
+  "/masjid-nurut-taqwa/icon-192.jpg",
+  "/masjid-nurut-taqwa/icon-512.jpg"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(OFFLINE_URL))
   );
 });
